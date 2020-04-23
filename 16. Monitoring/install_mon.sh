0c85102e6aa0acf3c701d061b3ea377a2a5c005b
unknown <Root@MGT01-10803.gtp.tn.corp>
2020-04-23 16:37:01 +0300

    change install_mon.sh

--- 16. Monitoring/install_mon.sh
+++ 16. Monitoring/install_mon.sh
@@ -1,19 +1,1 @@
 #! /bin/bash
-
-#install and configuring NTP
-yum install chrony
-systemctl enable chronyd
-systemctl start chronyd
-
-#configuring Firewall
-firewall-cmd --permanent --add-port=514/{tcp,udp}
-firewall-cmd --reload
-
-#configuring SELinux
-semanage port -m -t syslogd_port_t -p tcp 514
-semanage port -m -t syslogd_port_t -p udp 514
-
-#install rsyslog
-yum install rsyslog
-systemctl enable rsyslog
-systemctl start rsyslog
